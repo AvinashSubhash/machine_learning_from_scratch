507a4b88fea585c0474d2b3cc7aa0c126ab48a4c
AvinashSubhash <59025615+AvinashSubhash@users.noreply.github.com>
2020-03-30 22:30:34 +0530

    Addition of accuracy indicator and sample input to predict the output

--- regression/simple_linear_regression.py
+++ regression/simple_linear_regression.py
@@ -4,8 +4,16 @@ Created on Mon Mar 30 15:47:44 2020
 
 @author: Aiva
 """
+"""Simple linear regression program with the concept of reducing the squared errors
 
+   This program displays the resultant equation of the data sets given 
+   and shows the accuracy level of the algorithm with respect to the data sets"""
 import numpy as np
+se_line=0
+se_mean=0
+accuracy=0
+xcord=0
+ycord=0
 cords=np.zeros(2)
 size=int(input("Enter the number of training data:"))
 data=np.zeros((size,2))
@@ -16,6 +24,7 @@ for i in range(size):
     data[i][1]=int(cords[1])
 
 mean=(data.sum(axis=0))/size
+
 beta_upper=0
 beta_lower=0
 
@@ -27,4 +36,17 @@ beta_i=(beta_upper/beta_lower)
 beta_o=(mean[1]-(beta_i*mean[0])) 
 
 print("resulted equation is y=",beta_i,"x + ",beta_o,"\n")  
+
+for i in range(size):
+    se_line+=(data[i][1]-((beta_i*data[i][0])+beta_o))*(data[i][1]-((beta_i*data[i][0])+beta_o))
+    se_mean+=(data[i][1]-mean[1])*(data[i][1]-mean[1])
     
+accuracy=1-(se_line/se_mean)    
+
+print("The accuracy of the algorithm in this case is: ",accuracy*100,"%")
+"""Furthur it takes an input and predicts the output from the data analysis"""
+print("Enter the x cordinate data")
+xcord=int(input())
+ycord=(beta_i*xcord)+beta_o
+print("The predicted output is: ",ycord)
+
