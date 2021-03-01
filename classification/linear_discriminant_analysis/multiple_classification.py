b2e7c9f97c01430cfb2835c4ab0a566d644e8ec4
AvinashSubhash <avinashsubhash19@gmail.com>
2021-03-01 23:25:47 +0530

    Working on linear discriminant analysis

---
+++ classification/linear_discriminant_analysis/multiple_classification.py
@@ -0,0 +1,21 @@
+import numpy as np
+import random
+
+def data_creator(size,noise=5):
+    data = np.zeros((2,size))
+    for i in range(size):
+        data[0][i] = i
+        if i >= 2*(size/noise) and i <= 3*(size/noise):
+            if random.randint(1,100) > 65:
+                data[1][i] = 0
+            else:
+                data[1][i] = 1
+        elif i < 2*(size/noise):
+            data[1][i] = 1
+        else:
+            data[1][i] = 0
+    return data
+
+
+def analyser(data):
+    pass
